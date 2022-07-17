098547379954404dc530b63a831e09cdc9816846
yasmin <yasminmartinsdebrito@gmail.com>
2022-07-17 07:31:14 -0300

    feat:correcoes de pagina , adicionando uma pagina padrao, criar o styles

--- src/components/Pokemon/index.jsx
+++ src/components/Pokemon/index.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 
-const Pokemon = ({id, name, image, type}) => {
-    
-    return (
-        <div className="thumb-container">
-            <div className="number">
-                <small>#0{id}</small>
-            </div>
-            <img src={image} alt={name}/>
-            <div className="detail-wrapper">
-                <h3>{name}</h3>
-                <small>Type: {type}</small>
-            </div>
-        </div>
-    )
-}
+const Pokemon = ({ id, name, image, type }) => {
+  return (
+    <div className="thumb-container">
+      {image ? (
+        <img src={image} alt={name} />
+      ) : (
+        <img src="../../assets/img/pokemonpadrao.jpeg" alt={name} />
+      )}
+      <div className="number">
+        <small>#0{id}</small>
+      </div>
+      <div className="detail-wrapper">
+        <h3>{name}</h3>
+        <small>Type: {type}</small>
+      </div>
+    </div>
+  );
+};
 
-export default Pokemon
+export default Pokemon;
